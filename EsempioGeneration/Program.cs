92bc1358fb9c378ce9f3d947a7c2c78b57d3c6d7
robertolagreca <robertolagreca92@gmail.com>
2022-11-21 11:51:57 +0100

    Aggiungere i file di progetto.

---
+++ EsempioGeneration/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var risposta = Console.ReadLine();
+
+Console.WriteLine("La risposta dall'utente " + risposta);
